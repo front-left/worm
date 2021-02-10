3f66bc3f2755dc0b1a23cfe4358c8ae0e8e474ea
zack dove <zdove@live.co.uk>
2021-02-10 17:54:12 +0000

    added more

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var curX = window.innerWidth/2;
 var curY = window.innerHeight/2;
 var sx = 0;
 var sy = Math.PI/2;
+var circles = [];
+var alt = 0;
 
 function setup(){
 	
@@ -20,14 +22,41 @@ function draw(){
 	b = 1-b;
 	fill(255*b);
 	noStroke();
+	var colour;
 	if (b){
-		fill('#828282');
+		colour = '#828282';
 	} else {
-		fill('#1eff00');
+		colour = '#1eff00';
 	}
+	fill(colour);
 	sx += 0.05;
 	sy += 0.05;
-	curX += (mouseX - curX)/40 + sin(sx)*2;
-	curY += (mouseY - curY)/40 + sin(sy)*2;
-	ellipse(curX, curY, 80, 80);
+	var dx = mouseX - curX;
+	var dy = mouseY - curY;
+	curX += (dx)/60 + sin(sx*1.2);
+	curY += (dy)/60 + sin(sy*0.7);
+	var d = Math.sqrt(Math.pow(dx,2), Math.pow(dy,2));
+	// ellipse(curX, curY, 80, 80);
+	if (!(d < 100 && alt)){
+		
+		circles.push({
+			x: curX,
+			y: curY,
+			r: 4,
+			colour: colour
+		});
+	} else {
+		alt = 1-alt;
+	}
+	clear();
+	for (var i = 1; i < circles.length-1; i++){
+		
+		// circles[i].r += 7;
+		if (circles[i].r < 100){
+			circles[i].r += 2;
+		}
+		
+		fill(circles[i].colour)
+		ellipse(circles[i].x, circles[i].y, circles[i].r, circles[i].r);
+	}
 }
